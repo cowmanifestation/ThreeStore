64101d64f66f6d9a71dc2e274b3d343c79a44d64
Chenoa Siegenthaler <csiegenthaler@hedgeye.com>
2011-12-02 15:19:05 -0500

    Key now includes hexdigest, no longer contains 'http://' from url, has been escaped by CGI.

--- lib/ThreeStore.rb
+++ lib/ThreeStore.rb
@@ -1,23 +1,38 @@
 require 'open-uri'
 require 'aws/s3'
+require 'digest/md5'
 autoload :VERSION, "ThreeStore/version"
 
-# maybe a class:
-# def initalize(key, secret, bucket)
 class ThreeStore
   def initialize(key, secret, bucket)
     AWS::S3::Base.establish_connection!(:access_key_id => key, :secret_access_key => secret)
     @bucket = bucket
   end
 
-  def store_on_s3(url)
-    url.gsub!(/ /, '%20')
+  # Options: url (required),
+  #          content_type,
+  #          access_level
+  def store_on_s3(options)
+    # Deleting :url from options because we don't want it in the options that we pass to s3
+    # and removing some characters that aren't allowed in urls
+    url = options.delete(:url).gsub(/ /, '%20').gsub(/\^/, '%5E')
     file = open(url)
-    AWS::S3::S3Object.store(url, file.read, @bucket)
-  end
-    
 
-  # store method should return location of thing being stored
-  # maybe also add a hexdigest to the name
-  # def store(url)
+    key = create_key(url, file)
+
+    AWS::S3::S3Object.store(key, file, @bucket, :access => options[:access], :content_type => options[:content_type])
+
+    # Return location on s3
+    "http://s3.amazonaws.com/" + @bucket + "/" + key
+  end
+
+  def create_key(url, file)
+    key = CGI.escape(url.gsub(/http:\/\//, ''))
+    file_content = file.read
+
+    digest = Digest::MD5.hexdigest(file_content)
+    extname = File.extname(url)
+
+    key.gsub(extname, "-#{digest}#{extname}")
+  end
 end
--- spec/lib/ThreeStore_spec.rb
+++ spec/lib/ThreeStore_spec.rb
@@ -3,25 +3,41 @@ require 'spec_helper'
 describe ThreeStore do
   
   before(:all) do
-    @test_bucket = 'hedgeye-test-bucket-3'
+    @test_bucket = 'hedgeye-ent-bucket'
     @store = ThreeStore.new(ENV['S3_KEY'], ENV['S3_SECRET'], @test_bucket)
+    @url = "http://www2.hedgeye.com/assets/hedgeye-ui/button-sprite.png"
+    @s3_object_path = @store.store_on_s3(:url => @url, :content_type => 'image/png', :access => 'public-read')
   end
+
+#  after(:all) do
+#    AWS::S3::Bucket.find(@test_bucket).clear
+#  end
 
   it "should establish a connection with s3" do
     AWS::S3::Base.connected?.should be_true
   end
 
   it "should put a given file on s3" do
-    url = "http://app.quotemedia.com/quotetools/getChart.go?webmasterId=98924&symbol=COMP&chsym=Nasdaq%20Comp&chscale=5d&chtype=AreaChart&chfrmon=on&chfrm=ffffff&chbdron=on&chbdr=cccccc&chbg=ffffff&chbgch=ffffff&chln=465665&chfill=1B57AA&chfill2=c0c9d2&chgrdon=on&chgrd=cccccc&chton=on&chtcol=000000&chxyc=111111&chpcon=on&chpccol=ee0000&chmrg=2&chhig=250&chwid=380"
+    # TODO: test with this url (a quotemedia chart) - it has some issues
+    # url = "http://app.quotemedia.com/quotetools/getChart.go?webmasterId=98924&symbol=COMP&chsym=Nasdaq%20Comp&chscale=5d&chtype=AreaChart&chfrmon=on&chfrm=ffffff&chbdron=on&chbdr=cccccc&chbg=ffffff&chbgch=ffffff&chln=465665&chfill=1B57AA&chfill2=c0c9d2&chgrdon=on&chgrd=cccccc&chton=on&chtcol=000000&chxyc=111111&chpcon=on&chpccol=ee0000&chmrg=2&chhig=250&chwid=380"
 
-    @store.store_on_s3(url)
-    AWS::S3::S3Object.exists?(url, @test_bucket).should be_true
+    open(@s3_object_path).read.should == open(@url).read
   end
 
-  it "should function with a url containing spaces" do
-    url = "http://app.quotemedia.com/quotetools/getChart.go?webmasterId=98924&symbol=COMP&chsym=Nasdaq Comp&chscale=5d&chtype=AreaChart&chfrmon=on&chfrm=ffffff&chbdron=on&chbdr=cccccc&chbg=ffffff&chbgch=ffffff&chln=465665&chfill=1B57AA&chfill2=c0c9d2&chgrdon=on&chgrd=cccccc&chton=on&chtcol=000000&chxyc=111111&chpcon=on&chpccol=ee0000&chmrg=2&chhig=250&chwid=380"
+  it "should add an md5 hexdigest to the key" do
+    file_content = open(@url).read
+    digest = Digest::MD5.hexdigest(file_content)
 
-    @store.store_on_s3(url)
-    AWS::S3::S3Object.exists?(url, @test_bucket).should be_true
+    @s3_object_path.should match(digest)
+  end
+
+  # Regression test because quotemedia urls have spaces and carets for some reason.
+  it "should remove spaces and carets from the given url" do
+    url = "http://app.quotemedia.com/quotetools/getChart.go?webmasterId=500&symbol=^DOW&chsym=^DOW Comp&chscale=5d&chtype=AreaChart&chfrmon=on&chfrm=ffffff&chbdron=on&chbdr=cccccc&chbg=ffffff&chbgch=ffffff&chln=465665&chfill=1B57AA&chfill2=c0c9d2&chgrdon=on&chgrd=cccccc&chton=on&chtcol=000000&chxyc=111111&chpcon=on&chpccol=ee0000&chmrg=2&chhig=250&chwid=380"
+
+    s3_object_path = @store.store_on_s3(:url => url, :content_type => 'image/png', :access => 'public-read')
+
+    s3_object_path.should_not match(/ /)
+    s3_object_path.should_not match(/\^/)
   end
 end
